e961711864bfae4d1e4d3568099b33db9b191050
Jnbhuvan <jnbhuvan01@gmail.com>
2026-08-09 14:21:09 +0530

    created product.java class for product table in the db with id as long datatype

---
+++ src/main/java/com/app/ecom/Model/Product.java
@@ -0,0 +1,39 @@
+package com.app.ecom.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String productName;
+    private String description;
+    private Integer stockQuantity;
+    private BigDecimal price;
+    private String category;
+    private String imageUrl;
+    private Boolean active = true;
+
+    @CreationTimestamp
+    private LocalDateTime creationTime;
+
+    @UpdateTimestamp
+    private LocalDateTime updateTime;
+
+}
