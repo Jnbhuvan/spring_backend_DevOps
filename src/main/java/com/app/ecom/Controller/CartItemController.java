682ca38886a26a595141a21282aa5f10981c00f9
Jnbhuvan <jnbhuvan01@gmail.com>
2026-08-11 15:11:30 +0530

    Post request for CartItem creation

--- src/main/java/com/app/ecom/Controller/CartItemController.java
+++ src/main/java/com/app/ecom/Controller/CartItemController.java
@@ -2,12 +2,10 @@ package com.app.ecom.Controller;
 
 import com.app.ecom.Model.CartItem;
 import com.app.ecom.Services.CartItemService;
+import com.app.ecom.dto.CartItemRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +23,12 @@ public class CartItemController {
     @GetMapping("/cartitems")
     public ResponseEntity<List<CartItem>> getAllCartItems(){
         return new ResponseEntity<>(cartItemService.getAllCart(), HttpStatus.OK);
+    }
+
+    @PostMapping("/cartitems")
+    public ResponseEntity<?> addToCart(@RequestHeader("X-User-ID") String userId,
+                                       @RequestBody CartItemRequest cartItemRequest)
+    {   return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
