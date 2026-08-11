45b6b50b9ebfa5d5b9d7ded150e7b78b22990a54
Jnbhuvan <jnbhuvan01@gmail.com>
2026-08-11 13:08:33 +0530

    Created CartItemController to handle get all request and respond to the client

--- src/main/java/com/app/ecom/Controller/CartItemController.java
+++ src/main/java/com/app/ecom/Controller/CartItemController.java
@@ -1,4 +1,30 @@
 package com.app.ecom.Controller;
 
+import com.app.ecom.Model.CartItem;
+import com.app.ecom.Services.CartItemService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api")
 public class CartItemController {
+
+    CartItemService cartItemService;
+
+    public CartItemController(CartItemService cartItemService) {
+        this.cartItemService = cartItemService;
+    }
+
+    @GetMapping("/cartitems")
+    public ResponseEntity<List<CartItem>> getAllCartItems(){
+        return new ResponseEntity<>(cartItemService.getAllCart(), HttpStatus.OK);
+    }
+
 }
