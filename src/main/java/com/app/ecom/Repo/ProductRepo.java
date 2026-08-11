3a5a3550b47ee01571590fc42dadfac971481e61
Jnbhuvan <jnbhuvan01@gmail.com>
2026-08-11 10:48:55 +0530

    Created a Custom query method in the productrepo for filtering and searching products by keyword

--- src/main/java/com/app/ecom/Repo/ProductRepo.java
+++ src/main/java/com/app/ecom/Repo/ProductRepo.java
@@ -2,6 +2,8 @@ package com.app.ecom.Repo;
 
 import com.app.ecom.Model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +11,7 @@ import java.util.List;
 @Repository
 public interface ProductRepo extends JpaRepository<Product, Long> {
     List<Product> findByActiveTrue();
+
+    @Query("SELECT p FROM products WHERE p.active=true AND p.stockQuantity>0 AND LOWER(p.name) LIKE CONCAT('%',:keyword,'%')")
+    List<Product> searchProducts(@Param("keyword") String keyword);
 }
