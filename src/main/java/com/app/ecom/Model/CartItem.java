48f0240f927b1cc98278625d8983a37ce9f59dff
Jnbhuvan <jnbhuvan01@gmail.com>
2026-08-11 11:50:48 +0530

    Created CartItem class model for CartItem table creation in the DB

--- src/main/java/com/app/ecom/Model/CartItem.java
+++ src/main/java/com/app/ecom/Model/CartItem.java
@@ -1,4 +1,40 @@
 package com.app.ecom.Model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cartId;
+
+    @ManyToOne()
+    @JoinColumn(name="user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name="prod_id", nullable=false)
+    private Product product;
+
+    private BigDecimal price;
+
+    private Integer quantity;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }
