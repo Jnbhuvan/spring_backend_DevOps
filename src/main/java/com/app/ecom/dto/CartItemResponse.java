3e4bb8b3fd9928a57578ac3e6fbf4ad0319cce5e
Jnbhuvan <jnbhuvan01@gmail.com>
2026-08-11 15:10:51 +0530

    created response file

--- src/main/java/com/app/ecom/dto/CartItemResponse.java
+++ src/main/java/com/app/ecom/dto/CartItemResponse.java
@@ -1,4 +1,18 @@
 package com.app.ecom.dto;
 
+
+import com.app.ecom.Model.Product;
+import com.app.ecom.Model.User;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class CartItemResponse {
+
+    private Long prodId;
+
+     private Integer quantity;
 }
