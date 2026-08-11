ecf0855fc9fc8b9c8ac82f3f77cc8ce3bb1e2c72
Jnbhuvan <jnbhuvan01@gmail.com>
2026-08-11 13:07:26 +0530

    Created and added CartItemSErvices file for get all request handling

--- src/main/java/com/app/ecom/Services/CartItemService.java
+++ src/main/java/com/app/ecom/Services/CartItemService.java
@@ -1,4 +1,21 @@
 package com.app.ecom.Services;
 
+import com.app.ecom.Model.CartItem;
+import com.app.ecom.Repo.CartItemRepo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CartItemService {
+
+    CartItemRepo cartItemRepo;
+
+    public CartItemService(CartItemRepo cartItemRepo) {
+        this.cartItemRepo = cartItemRepo;
+    }
+
+    public List<CartItem> getAllCart(){
+        return cartItemRepo.findAll();
+    }
 }
