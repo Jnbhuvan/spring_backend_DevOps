97dff9953c2d34e8607ecbe022ff481b8401ad13
Jnbhuvan <jnbhuvan01@gmail.com>
2026-08-12 22:01:31 +0530

    Created OrderResponse file with necessary fields

--- src/main/java/com/app/ecom/dto/OrderResponse.java
+++ src/main/java/com/app/ecom/dto/OrderResponse.java
@@ -1,4 +1,27 @@
 package com.app.ecom.dto;
 
+
+import com.app.ecom.Model.OrderItem;
+import com.app.ecom.Model.OrderStatus;
+import com.app.ecom.Model.User;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
 public class OrderResponse {
+
+    private Long id;
+
+    private BigDecimal totalAmount;
+
+    private OrderStatus status;
+
+    private List<OrderItemDTO> items;
+
+    private LocalDateTime createdAt;
+
 }
