a0bd52676e0d4ad0bee079273fd1588ccb1a47e4
Jnbhuvan <jnbhuvan01@gmail.com>
2026-08-12 14:56:57 +0530

    Provided the necessary fields for the OrderItem class

--- src/main/java/com/app/ecom/Model/OrderItem.java
+++ src/main/java/com/app/ecom/Model/OrderItem.java
@@ -1,4 +1,33 @@
 package com.app.ecom.Model;
 
-public class OrderItems {
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="orderitems_table")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    private Integer quantity;
+
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name="order_id", nullable = false)
+    private Order order;
 }
