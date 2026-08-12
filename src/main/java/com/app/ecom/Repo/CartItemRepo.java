8100b37b21fdf4e0b7efe5c5ff4546e0e478ef90
Jnbhuvan <jnbhuvan01@gmail.com>
2026-08-12 12:40:44 +0530

    create findByUser method in the cartItemRepo

--- src/main/java/com/app/ecom/Repo/CartItemRepo.java
+++ src/main/java/com/app/ecom/Repo/CartItemRepo.java
@@ -5,7 +5,11 @@ import com.app.ecom.Model.Product;
 import com.app.ecom.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CartItemRepo extends JpaRepository<CartItem, Long> {
     CartItem findByUserAndProduct(User user, Product product);
     Long deleteByUserAndProduct(User user, Product product);
+
+    List<CartItem> findByUser(User user);
 }
