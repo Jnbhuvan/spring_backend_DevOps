89151033ca41c140d0e4cc028b3071d3824949cd
Jnbhuvan <jnbhuvan01@gmail.com>
2026-08-12 22:04:17 +0530

    Created a Constructor for Injecting Repo Dependency to the service

--- src/main/java/com/app/ecom/Services/OrderService.java
+++ src/main/java/com/app/ecom/Services/OrderService.java
@@ -1,7 +1,14 @@
 package com.app.ecom.Services;
 
+import com.app.ecom.Repo.OrderRepo;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
+
+    OrderRepo orderRepo;
+
+    public OrderService(OrderRepo orderRepo) {
+        this.orderRepo = orderRepo;
+    }
 }
