fb0696b15d06031782812dc4e12e222347c9f23a
Jnbhuvan <jnbhuvan01@gmail.com>
2026-08-12 22:01:58 +0530

    Created OrderItemDTO file with necessary fields

--- src/main/java/com/app/ecom/dto/OrderItemDTO.java
+++ src/main/java/com/app/ecom/dto/OrderItemDTO.java
@@ -1,4 +1,18 @@
 package com.app.ecom.dto;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class OrderItemDTO {
+
+    private Long id;
+
+    private Long productId;
+
+    private Integer quantity;
+
+    private BigDecimal price;
+
 }
