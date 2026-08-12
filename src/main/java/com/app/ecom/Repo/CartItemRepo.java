8dcb39f9c147cd7b2df023909cbb1979bffb1080
Jnbhuvan <jnbhuvan01@gmail.com>
2026-08-12 09:06:11 +0530

    Created findByUserAndProduct method in the CartItemRepo

--- src/main/java/com/app/ecom/Repo/CartItemRepo.java
+++ src/main/java/com/app/ecom/Repo/CartItemRepo.java
@@ -1,7 +1,10 @@
 package com.app.ecom.Repo;
 
 import com.app.ecom.Model.CartItem;
+import com.app.ecom.Model.Product;
+import com.app.ecom.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartItemRepo extends JpaRepository<CartItem, Long> {
+    CartItem findByUserAndProduct(User user, Product product);
 }
