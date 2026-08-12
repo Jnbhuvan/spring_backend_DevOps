f28f4a04eec2a9aa5c4d33b50ea77f0b076e20b6
Jnbhuvan <jnbhuvan01@gmail.com>
2026-08-12 22:04:46 +0530

    Created a Constructor for Injecting service Dependency to the controller

--- src/main/java/com/app/ecom/Controller/OrderController.java
+++ src/main/java/com/app/ecom/Controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.app.ecom.Controller;
 
 
+import com.app.ecom.Services.OrderService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,4 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 @RequestMapping("/api/orders")
 public class OrderController {
+
+    OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+
 }
